4646b921a28f4b2b94496118210ba431f355645b
Carson Sanders <44068699+carsonsanders@users.noreply.github.com>
2021-01-22 20:08:04 -0800

    added equiping functionality

--- UnityRPG/Assets/Scripts/Inventory/Inventory.cs
+++ UnityRPG/Assets/Scripts/Inventory/Inventory.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    [SerializeField] private Transform _rightHand;
+    
     private List<Item> _items = new List<Item>();
     private Transform _itemRoot;
+    
+    public Item ActiveItem { get; private set; }
 
     private void Awake()
     {
@@ -23,5 +27,9 @@ public class Inventory : MonoBehaviour
     private void Equip(Item item)
     {
         Debug.Log($"Equipped Item {item.gameObject.name}");
+        item.transform.SetParent(_rightHand);
+        item.transform.localPosition = Vector3.zero;
+        item.transform.localRotation = Quaternion.identity;
+        ActiveItem = item;
     }
 }
