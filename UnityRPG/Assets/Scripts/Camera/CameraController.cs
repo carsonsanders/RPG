2e8785d0bc9f6a35c57802d71fb96cdf2d982ac2
Carson Sanders <44068699+carsonsanders@users.noreply.github.com>
2021-01-23 15:12:18 -0800

    Added Camera controller w/tilt

---
+++ UnityRPG/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float _tilt;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float mouseRotation = Input.GetAxis("Mouse Y");
+        _tilt = Mathf.Clamp(_tilt - mouseRotation, -15f, 15f);
+        transform.localRotation = Quaternion.Euler(_tilt,0f,0f);
+    }
+}
