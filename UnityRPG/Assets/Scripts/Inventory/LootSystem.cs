6085e3d9a63d5c51f70fb364e7685ba4c937f9d1
Carson Sanders <44068699+carsonsanders@users.noreply.github.com>
2021-01-26 15:04:31 -0800

    Refactored Loot System to use Unity's addressable system

---
+++ UnityRPG/Assets/Scripts/Inventory/LootSystem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class LootSystem : MonoBehaviour
+{
+    [SerializeField] private AssetReference _lootItemHolderPrefab;
+    
+    private static LootSystem _instance;
+
+    private void Awake()
+    {
+        if(_instance != null)
+            Destroy(gameObject);
+        else
+            _instance = this;
+        
+    }
+
+    public static void Drop(Item item, Transform droppingTransform)
+    {
+        _instance.StartCoroutine(_instance.DropAsync(item, droppingTransform));
+        
+    }
+
+    private IEnumerator DropAsync(Item item, Transform droppingTransform)
+    {
+        var operation = _lootItemHolderPrefab.InstantiateAsync();
+        yield return operation;
+        
+        var lootItemHolder = operation.Result.GetComponent<LootItemHolder>();;
+        lootItemHolder.TakeItem(item);
+
+        Vector2 randomCirclePoint = UnityEngine.Random.insideUnitCircle * 3f;
+        Vector3 randomPosition = droppingTransform.position + new Vector3(randomCirclePoint.x, 0, randomCirclePoint.y);
+
+        lootItemHolder.transform.position = randomPosition;
+    }
+
+}
