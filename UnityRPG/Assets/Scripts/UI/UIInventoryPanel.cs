d8f41bf22568931ed957ca1e7f4af87a8c6685e0
Carson Sanders <44068699+carsonsanders@users.noreply.github.com>
2021-01-27 20:04:50 -0800

    TDD Inventory UI test for 25 slots

---
+++ UnityRPG/Assets/Scripts/UI/UIInventoryPanel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class UIInventoryPanel : MonoBehaviour
+{
+    private UIInventorySlot[] _slots;
+
+
+    private void Awake()
+    {
+        _slots = GetComponentsInChildren<UIInventorySlot>();
+    }
+
+    public int SlotCount => _slots.Length;
+}
