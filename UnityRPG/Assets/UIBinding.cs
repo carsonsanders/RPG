c8941e390a6dbe66cd4ddccef42c2efdaca3be3f
Carson Sanders <44068699+carsonsanders@users.noreply.github.com>
2021-01-27 21:33:09 -0800

    Inventory UI Binds to player inventory

---
+++ UnityRPG/Assets/UIBinding.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBinding : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        var player = FindObjectOfType<Player>();
+        while (player == null)
+        {
+            yield return null;
+            player = FindObjectOfType<Player>();
+        }
+
+        GetComponent<UIInventoryPanel>().Bind(player.GetComponent<Inventory>());
+    }
+    
+}
