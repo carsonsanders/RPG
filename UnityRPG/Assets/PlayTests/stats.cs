b28276df06eeea39d1b1188c7523c1d5b3abca87
Carson Sanders <44068699+carsonsanders@users.noreply.github.com>
2021-01-30 17:20:29 -0800

    TDD Stats! Add/Remove tests+implementation

---
+++ UnityRPG/Assets/PlayTests/stats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using NUnit.Framework;
+using UnityEngine;
+
+public class stats
+{
+    [Test]
+    public void can_add()
+    {
+        Stats stats = new Stats();
+        stats.Add(StatType.MoveSpeed, 5f);
+        Assert.AreEqual(5f, stats.Get(StatType.MoveSpeed));
+        stats.Add(StatType.MoveSpeed, 5f);
+        Assert.AreEqual(10f, stats.Get(StatType.MoveSpeed));
+    }
+    
+    [Test]
+    public void can_remove()
+    {
+        Stats stats = new Stats();
+        stats.Add(StatType.MoveSpeed, 5f);
+        Assert.AreEqual(5f, stats.Get(StatType.MoveSpeed));
+        
+        stats.Remove(StatType.MoveSpeed, 5f);
+        Assert.AreEqual(0f, stats.Get(StatType.MoveSpeed));
+    }
+}
---
+++ UnityRPG/Assets/Scripts/Stats/Stats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Stats
+{
+    private Dictionary<StatType, float> _stats = new Dictionary<StatType, float>();
+
+    public void Add(StatType statType, float value)
+    {
+        if (_stats.ContainsKey(statType))
+            _stats[statType] += value;
+        else
+            _stats[statType] = value;
+    }
+
+    public double Get(StatType statType)
+    {
+        return _stats[statType];
+    }
+
+    public void Remove(StatType statType, float value)
+    {
+        if (_stats.ContainsKey(statType))
+            _stats[statType] -= value;
+    }
+}
