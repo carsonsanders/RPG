ba6d943afa1ad849106ffd27cfae6f23cca29b71
Carson Sanders <44068699+carsonsanders@users.noreply.github.com>
2021-02-02 20:58:41 -0800

    Character Creation Stat Allocation implemented

---
+++ UnityRPG/Assets/Scripts/UI/StatModifier.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatModifier : MonoBehaviour
+{
+    private int _statValue;
+    private TMPro.TextMeshProUGUI _valueString;
+    private Button _plusButton;
+    private Button _minusButton;
+
+    public void AddStat()
+    {
+        _statValue += 1;
+        updateText();
+    }
+
+    public void MinusStat()
+    {
+        _statValue -= 1;
+        updateText();
+    }
+
+    public int GetStat()
+    {
+        return _statValue;
+    }
+    
+
+    public Button GetPlusButton()
+    {
+        return _plusButton;
+    }
+
+    public Button GetMinusButton()
+    {
+        return _minusButton;
+    }
+
+    private void updateText()
+    {
+        _valueString.text = "[ " + _statValue + " ]";
+    }
+    
+
+    private void OnEnable()
+    {
+        _valueString = GetComponent<TMPro.TextMeshProUGUI>();
+        Button[] buttons = GetComponentsInChildren<Button>();
+        _statValue = 2;
+        _plusButton = buttons[0];
+        _minusButton = buttons[1];
+    }
+}
---
+++ UnityRPG/Assets/Scripts/UI/StatsManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class StatsManager : MonoBehaviour
+{
+    [SerializeField] private TMPro.TextMeshProUGUI _characterPointsText;
+    
+    private int _characterPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterPoints = 30;
+        updateText();
+
+        StatModifier[] statModifiers = GetComponentsInChildren<StatModifier>();
+
+        foreach (var statModifier in statModifiers)
+        {
+            statModifier.GetPlusButton().onClick.AddListener(()=>addStat(statModifier));
+            statModifier.GetMinusButton().onClick.AddListener(()=>minusStat(statModifier));
+        }
+        
+    }
+
+    private void addStat(StatModifier sm)
+    {
+        if (_characterPoints > 0 && sm.GetStat() < 10)
+        {
+            sm.AddStat();
+            _characterPoints -= 1;
+        }
+
+        updateText();
+    }
+
+    private void minusStat(StatModifier sm)
+    {
+        if (sm.GetStat() > 2)
+        {
+            sm.MinusStat();
+            _characterPoints += 1;
+        } 
+        
+        updateText();
+    }
+
+    private void updateText()
+    {
+        _characterPointsText.text = "CHARACTER POINTS: [ " + _characterPoints + " ]";
+    }
+
+   
+}
